d69ef8dd45ebd1ed7b31013835ccc230912efed5
nastex21 <nasax2000@gmail.com>
2021-01-12 17:02:03 -0600

    Reverted back to original process of data gathering without JQuery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,12 +5,47 @@ class HomeController < ApplicationController
     created_games = CreatedGame.all
     @games = steam_games + created_games
 
-    respond_to do |format|
-      format.html { render(:text => "not implemented") }
-      format.js
-    end
+    @search = find_game(params[:game])
 
-  end
+    if @search.present?
+        @results = @search["results"]
+        @results = JSON.parse(@search)
     
+        respond_to do |format|
+          format.html { render(:text => "not implemented") }
+          format.js
+        end
+
+        unless @results
+            flash[:alert] = 'Sorry, game not found'
+            return render action: :index
+        end
+    end
+  end
+
+private
+
+def request_api(url)
+    response = Excon.get(
+        url,
+        :headers => {
+        'X-RapidAPI-Host' => URI.parse(url).host,
+        'X-RapidAPI-Key' => ENV['RAPID_KEY']
+        }
+    )
+
+return nil if response.status != 200
+
+
+return response.body
+end
+
+def find_game(name)
+    if name.present?
+        url = "https://rawg-video-games-database.p.rapidapi.com/games?search=#{CGI.escape(name)}&page_size=10"
+        request_api(url)
+    end
+end
+  
 
 end
