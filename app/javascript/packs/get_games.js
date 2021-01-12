24c71f5337f005e530835233777b9a9e7532108c
Tony <nasax2000@gmail.com>
2021-01-11 20:27:00 -0600

    commit, AJAX worked

--- app/javascript/packs/get_games.js
+++ app/javascript/packs/get_games.js
@@ -1,33 +1,22 @@
-/* $('h2').on('click', '.action', function() {
-    //console.log("data", selectedHuddleIds());
-    $.ajax('#{organize_process_path}', {
-      data: selectedHuddleIds(),
-      contentType: 'application/json',
-      dataType: 'json'
-    });
-  });
-
-  function selectedHuddleIds() {
-    var ids = [], selected_cards = $('.card.selected');
-    selected_cards.each(function() {
-      ids.push( $(this).data('huddle-id') );
-    })
-    return JSON.stringify({ 'huddle_ids' : ids });
-  } */
-
 document.addEventListener("click", function (ev) {
-  if (ev.target.id == "get_games_button") {
-    console.log("it works");
-  }
-});
-/* import { hello } from "./hello";
 
-function greet_user(last_name, first_name) {
-  hello(last_name + " " + first_name);
-}
-$(document).ready(function () {
-  $("button#greet-user-button").on("click", function () {
-    greet_user("Dire", "Strait");
-  });
-});
- */
+    var url = "https://rawg-video-games-database.p.rapidapi.com/games?search=Mario&page_size=10";
+    if (ev.target.id == "get_games_button") {
+        const settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": url,
+            "method": "GET",
+            "contentType": 'application/json',
+            "dataType": 'json',
+            "headers": {
+                "x-rapidapi-key": process.env.RAPID_KEY,
+                "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+            }
+        };
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    }
+})
