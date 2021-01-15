ebcb8c4d1d1328603901cf46f91a5e009de41fbc
nastex21 <nasax2000@gmail.com>
2021-01-15 14:59:03 -0600

    Search input media queries mostly set

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
 
     @games = steam_games + created_games
 
-    @search = find_game(params[:game], params[:platform_list], params[:store_list])
+    find_game(params[:game], params[:platform_list], params[:store_list])
   end
 
 private
@@ -33,14 +33,14 @@ private
       if name.present? && platform.blank? && store.blank?
           puts 'inside first conditional'
           url = "https://rawg-video-games-database.p.rapidapi.com/games?search=#{CGI.escape(name)}&page_size=10"
-          request_api(url)
+          @games = request_api(url)
       elsif name.present? && platform.present? && store.blank?
           puts 'inside second conditional'
           url = "https://rawg-video-games-database.p.rapidapi.com/games?search=#{CGI.escape(name)}&platforms=#{CGI.escape(platform)}&page_size=10"
-          request_api(url)
+          @platform_games = request_api(url)
       elsif name.present? && store.present? 
           url = "https://rawg-video-games-database.p.rapidapi.com/games?search=#{CGI.escape(name)}&stores=#{CGI.escape(store)}&page_size=10"
-          request_api(url)
+          @store_games = request_api(url)
       elsif name.blank? && store.present? || name.blank? && platform.present?
           flash[:alert] = 'Please enter name of game'
           return render action: :index
