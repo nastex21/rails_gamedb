4ec7d0d7ea4ce33b60c6fa6f80116fd4e780c74c
nastex21 <nasax2000@gmail.com>
2021-02-05 22:41:10 -0600

    commit, adding some changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :steam_games
          has_many :created_games
 end
